48098e1316e9816dd88db50378b1c965be6ad548
wenderson jonatha <wendersonkw1@gmail.com>
2025-11-07 23:38:30 -0300

    novo

---
+++ src/test/java/Sistema/de/Gestao/Hospitalar/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Sistema.de.Gestao.Hospitalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
